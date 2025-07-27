91b5b89b591665d9edb89b7ee0409f5a96628002
ヴ <rakha2728@gmail.com>
2025-07-27 11:58:05 +0800

    Add all source code in files for completed project

--- project/4-polygon-area-calculator/polygon.py
+++ project/4-polygon-area-calculator/polygon.py
@@ -1,1 +1,84 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
 
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_width(self):
+        return self.width
+
+    def get_height(self):
+        return self.height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return (2 * self.width) + (2 * self.height)
+
+    def get_diagonal(self):
+        return (self.width**2 + self.height**2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+
+        line = "*" * self.width
+        lines = [line for _ in range(self.height)]
+        picture = "\n".join(lines)
+        return picture + "\n"
+
+    def get_amount_inside(self, shape):
+        w = self.width // shape.width
+        h = self.height // shape.height
+
+        return w * h
+
+    def __str__(self):
+        return (
+            "Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")"
+        )
+
+
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def set_width(self, width):
+        self.width = width
+        self.height = width
+
+    def set_height(self, height):
+        self.width = height
+        self.height = height
+
+    def __str__(self):
+        return "Square(side=" + str(self.width) + ")"
+
+
+rect = Rectangle(10, 5)
+print(rect.get_area())
+rect.set_height(3)
+print(rect.get_perimeter())
+print(rect)
+print(rect.get_picture())
+
+sq = Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
+print(sq.get_picture())
+
+rect.set_height(8)
+rect.set_width(16)
+print(rect.get_amount_inside(sq))
